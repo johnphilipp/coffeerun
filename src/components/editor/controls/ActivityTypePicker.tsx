94d940c44ff6c5d0b5d4f70d48de9d23aa77ba82
Philipp John <philipp.john98@gmail.com>
2025-06-11 08:34:49 +0200

    add toggle all checkbox to activity type picker

--- src/components/editor/controls/ActivityTypePicker.tsx
+++ src/components/editor/controls/ActivityTypePicker.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Popover,
   PopoverContent,
@@ -8,10 +9,13 @@ import { cn } from "@/lib/utils";
 import { useActivityStore } from "@/store/activityStore";
 import { useControlsStore } from "@/store/controlsStore";
 import { Check, Dumbbell } from "lucide-react";
-import React from "react";
 
 export default function ActivityTypePicker() {
-  const { selectedActivityTypes, toggleActivityType } = useControlsStore();
+  const {
+    selectedActivityTypes,
+    toggleActivityType,
+    setSelectedActivityTypes,
+  } = useControlsStore();
   const { validActivityTypes } = useActivityStore();
 
   return (
@@ -19,7 +23,24 @@ export default function ActivityTypePicker() {
       <PopoverTrigger>
         <Dumbbell className="text-primary-foreground" style={{ scale: 1.2 }} />
       </PopoverTrigger>
-      <PopoverContent title="Activity Types">
+      <PopoverContent className="min-w-54">
+        <div className="flex items-center justify-between border-b -mx-4 px-4 mb-4 pb-2 -mt-1">
+          <h4 className="font-medium text-popover-foreground">
+            Activity Types
+          </h4>
+          <Checkbox
+            className="mr-4"
+            checked={selectedActivityTypes.length === validActivityTypes.length}
+            onCheckedChange={() => {
+              if (selectedActivityTypes.length === validActivityTypes.length) {
+                setSelectedActivityTypes([]);
+              } else {
+                setSelectedActivityTypes(validActivityTypes);
+              }
+            }}
+          />
+        </div>
+
         <div className="grid gap-2">
           {validActivityTypes.map((activityType) => {
             const isSelected = selectedActivityTypes.includes(activityType);
